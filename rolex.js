a391e48193799721877b1473c2b7756baf0ac63f
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2023-12-12 13:50:25 +0300

    Added function

--- rolex.js
+++ rolex.js
@@ -18,6 +18,20 @@ function handleImageUpload() {
 }
 
 function displayRandomImage() {
-    // Your existing code to display random images
-    // ...
+    var images = [
+        'vitunguu(images)/590_1.jpg',
+        'vitunguu(images)/chapatti-mayai-recipe-main-photo.jpg',
+        'vitunguu(images)/download.jpeg',
+        'vitunguu(images)/chapo-mayai-recipe-step-7-photo.jpg'
+        // Add more image paths as needed
+    ];
+
+    // Get a random index
+    var randomIndex = Math.floor(Math.random() * images.length);
+
+    // Get the image element by its ID
+    var imageElement = document.getElementById('randomImage');
+
+    // Set the source attribute of the image element to display the random image
+    imageElement.src = images[randomIndex];
 }
